191a773e977c9da209a75817078102f2528888a8
dreamcat4 <dreamcat4@gmail.com>
2018-02-23 21:52:04 +0000

    add nodejs dependencies for some very light postcss processing

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+
+var gulp = require('gulp');
+var extname = require('gulp-extname');
+
+var postcss = require('gulp-postcss');
+var simpleVars = require('postcss-simple-vars');
+var colorFunction = require("postcss-color-function");
+var customMedia = require("postcss-custom-media");
+
+
+gulp.task('postcss', function () {
+  var processors = [
+    simpleVars(),
+    colorFunction(),
+    customMedia()
+  ];
+  return gulp.src('./static/**/*.postcss')
+	.pipe(extname('.css'))
+    .pipe(postcss(processors))
+	.pipe(gulp.dest('./static'));
+});
+
+
+    // .pipe(postcss(processors).on('error', gutil.log))
+
+
+// Watch asset folder for changes
+gulp.task("watch", ["postcss"], function () {
+    gulp.watch("static/**/*.postcss", ["postcss"])
+})
+
+
