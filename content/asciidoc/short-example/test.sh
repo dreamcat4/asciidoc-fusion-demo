8094bd795ee96654477ff2a3a880e754667e56e9
dreamcat4 <dreamcat4@gmail.com>
2018-03-09 09:03:01 +0000

    Demo content restructure - Remove pages

--- content/asciidoc/short-example/test.sh
+++
@@ -1,8 +0,0 @@
-	#!/bin/bash
-
-	i=0
-	while [ $i -lt 2000000 ] 
-	do
-	  echo offset=$i
-	  i=$(($i+1000))
-	done
--- content/section1/s1_1/s_1_2a/_index.md
+++
@@ -1,7 +0,0 @@
-+++
-date = "2017-06-05T02:06:13+02:00"
-title = "Section S1/2a"
-
-+++
-
-Some content for S1/2a.
